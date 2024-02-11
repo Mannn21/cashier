8562b0df1b121405ec99b55946dd7f0480fe5538
Mannn21 <aimannurrofi21@gmail.com>
2024-02-11 17:44:13 +0700

    update order list api

--- src/app/api/order_lists/route.js
+++ src/app/api/order_lists/route.js
@@ -1,19 +1,24 @@
 import { NextResponse } from "next/server";
-import { doc, setDoc, getDocs, collection, where, query } from "firebase/firestore";
+import { doc, setDoc, getDoc, getDocs, collection, where, query } from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid"
 import { db } from "@/app/firebase";
 
 export const POST = async req => {
-    const { orders, total_price, table_id, costumer_name, date, order_time, cashier_id, total_item, total_discount, total_payment, message } = await req.json();
-    if(!orders, total_price, table_id, costumer_name, date, order_time, cashier_id, total_item, total_discount, total_payment, message) {
+    const { orders, table_id, costumer_name, date, order_time, cashier_id, total_item, total_discount, total_payment, message } = await req.json();
+    if(!orders, !table_id, !costumer_name, !date, !order_time, !cashier_id, !total_item, !total_payment) {
         return NextResponse.json(
             {message: "Harap lengkapi data input"},
             {status: 400, statusText: "Bad Request"}
         )
     }
     try {
-        const id = uuidv4();
-        
+        const cashierRef = doc(db, "employees", cashier_id);
+        const searchCashier = await getDoc(cashierRef);
+        return NextResponse.json(
+            {message: searchCashier.data()},
+            {status: 200, statusText: "Ok"}
+        )
+
     } catch (error) {
         return NextResponse.json(
             {message: "Kesalahan pada server"},
