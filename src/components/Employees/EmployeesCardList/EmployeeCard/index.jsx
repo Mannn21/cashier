328dbeec55981ddedf24432f1cb331f20da660e7
Mannn21 <aimannurrofi21@gmail.com>
2024-03-09 09:24:54 +0700

    update employees card list

--- src/components/Employees/EmployeesCardList/EmployeeCard/index.jsx
+++ src/components/Employees/EmployeesCardList/EmployeeCard/index.jsx
@@ -1,11 +1,61 @@
-const EmployeeCard = () => {
-    return (
-        <div className="w-full h-auto">
-            <div className="w-full h-auto p-2 bg-color-primer rounded-md">
-                <h1>Employee Card</h1>
-            </div>
-        </div>
-    )
-}
+import Image from "next/image";
 
-export default EmployeeCard
+const EmployeeCard = ({data}) => {
+	return (
+		<div className="w-full h-auto">
+			<div className="w-full h-auto p-2 bg-color-primer rounded-md shadow-md">
+				<div className="w-full h-auto p-1 flex flex-col gap-3 justify-start items-center">
+					<div className="w-full h-auto flex flex-row justify-start items-center gap-2">
+						<div className="relative w-[80px] h-[80px] rounded-full overflow-hidden bg-color-primer ring-2 ring-color-primer">
+							<Image
+								src="https://placehold.jp/80x80.png"
+								alt="Profile"
+								priority
+                                fill
+                                sizes="100%"
+								className="object-center object-cover w-full h-full"
+							/>
+						</div>
+
+						<div className="w-[calc(100%-80px)] h-auto flex flex-col justify-start items-start gap-1">
+							<h3 className="text-base font-semibold tracking-wide text-color-dark">
+								{data.name}
+							</h3>
+							<span className="text-sm font-medium tracking-wide text-color-tersier3">
+								{data.username}
+							</span>
+						</div>
+					</div>
+					<div className="w-full h-auto flex flex-col justify-start items-start gap-3">
+						<div className="w-full h-auto flex flex-row gap-1 justify-start items-center">
+							<h4 className="text-sm font-semibold tracking-wide text-color-dark">
+								Id Pegawai:{" "}
+							</h4>
+							<span className="text-sm font-semibold tracking-wide text-color-tersier3">
+								{data.zipcode}
+							</span>
+						</div>
+						<div className="w-full h-auto flex flex-row gap-1 justify-start items-center">
+							<h4 className="text-sm font-semibold tracking-wide text-color-dark">
+								Email:{" "}
+							</h4>
+							<span className="text-sm font-semibold tracking-wide text-color-tersier3">
+								{data.email}
+							</span>
+						</div>
+						<div className="w-full h-auto flex flex-row gap-1 justify-start items-center">
+							<h4 className="text-sm font-semibold tracking-wide text-color-dark">
+								Mulai Bekerja:{" "}
+							</h4>
+							<span className="text-sm font-semibold tracking-wide text-color-tersier3">
+								05 Maret 2024
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default EmployeeCard;
--- src/components/Employees/EmployeesCardList/index.jsx
+++ src/components/Employees/EmployeesCardList/index.jsx
@@ -1,15 +1,39 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import EmployeeCard from "./EmployeeCard";
 
 const EmployeesCardList = () => {
+    const [ employeesList, setEmployeesList ] = useState([]);
+
+    const fetchApi = async() => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+            method: "GET",
+            cache: "no-store"
+        })
+        const res = await response.json();
+        setEmployeesList(res)
+        console.time("Fetching Users Data")
+        console.log("%cFetching Started...", "color: crimson; background: black; padding: 5px");
+        console.table(res);
+        console.log("%cFetching Done...", "color: teal; background: black; padding: 5px");
+        console.timeEnd("Fetching Users Data")
+    }
+    
+    useEffect(() => {
+        fetchApi();
+    }, [])
+    
     return (
         <div className="w-full h-auto p-2">
-            <div className="w-full h-auto grid grid-cols-3">
-                <EmployeeCard />
-                <EmployeeCard />
-                <EmployeeCard />
-                <EmployeeCard />
-                <EmployeeCard />
-                <EmployeeCard />
+            <div className="w-full h-auto grid grid-cols-4 gap-6">
+                {
+                    employeesList?.map(data => {
+                        return (
+                            <EmployeeCard key={data.id} data={data} />
+                        )
+                    })
+                }
             </div>
         </div>
     )
